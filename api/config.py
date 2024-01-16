35983423f84c1fb681e55a7278ae5e8a2cb51dfc
Edgar Méndez <mdzredgar@gmail.com>
2024-01-16 16:55:06 -0600

    Agregando pruebas unitarias

--- api/config.py
+++ api/config.py
@@ -8,6 +8,7 @@ DB_PASSWD = os.getenv("PG_PASSWORD")
 DB_NAME = os.getenv("PG_DBNAME")
 DB_HOST = os.getenv("PG_HOST")
 DB_PORT = os.getenv("PG_PORT")
+DB_NAME_TEST = os.getenv("DBNAME_TEST")
 
 
 class Config:
@@ -20,6 +21,13 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATION = False
 
 
+class TestConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME_TEST}"
+    SQLALCHEMY_TRACK_MODIFICATION = False
+
+
 config = {
+    'test': TestConfig,
     'development': DevelopmentConfig
 }
