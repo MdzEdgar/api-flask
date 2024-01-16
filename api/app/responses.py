07c8d217b3474b8df50b3f4a182295994a6665f2
Edgar Méndez <mdzredgar@gmail.com>
2024-01-16 14:13:40 -0600

    Creación de funciones de respuesta.

--- api/app/responses.py
+++ api/app/responses.py
@@ -1,6 +1,17 @@
 from flask import jsonify
 
 
+def not_found():
+    return jsonify(
+        {
+            'success': False,
+            'data': {},
+            'message': 'Resource not found',
+            'code': 404
+        }
+    ), 404
+
+
 def response(data):
     return jsonify(
         {
