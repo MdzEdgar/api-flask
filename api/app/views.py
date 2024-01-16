8d242e7da650dcf5753fc0db94475b317cbca375
Edgar Méndez <mdzredgar@gmail.com>
2024-01-16 14:14:17 -0600

    Rutas GET agregadas.

--- api/app/views.py
+++ api/app/views.py
@@ -1,20 +1,29 @@
 from flask import Blueprint
 
+from .models.task import Task
 from .responses import response
+from .responses import not_found
 
 api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
 
 
 @api_v1.route('/tasks', methods=['GET'])
 def get_tasks():
-    return response({
-        'mensaje': 'Nuevo mensaje'
-    })
+    tasks = Task.query.all()
+
+    return response([
+        task.serialize() for task in tasks
+    ])
 
 
 @api_v1.route('/tasks/<id>', methods=['GET'])
-def get_task():
-    pass
+def get_task(id):
+    task = Task.query.filter_by(id=id).first()
+
+    if task is None:
+        return not_found()
+
+    return response(task.serialize())
 
 
 @api_v1.route('/tasks', methods=['POST'])
