9176a20b137859cb2d698c52eaa7c69f4b180b76
Edgar Méndez <mdzredgar@gmail.com>
2024-01-23 15:19:21 -0600

    Decoradores completos.

--- api/app/views.py
+++ api/app/views.py
@@ -12,14 +12,13 @@ api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
 
 def set_task(function):
     def wrap(*args, **kwargs):
-        with current_app.app_context():
-            id = kwargs.get('id', 0)
-            task = Task.query.filter_by(id=id).first()
+        id = kwargs.get('id', 0)
+        task = Task.query.filter_by(id=id).first()
 
-            if task is None:
-                return not_found()
+        if task is None:
+            return not_found()
 
-            return function(task)
+        return function(task)
 
     wrap.__name__ = function.__name__
     return wrap()
@@ -64,12 +63,8 @@ def create_task():
 
 
 @api_v1.route('/tasks/<id>', methods=['PUT'])
-def update_task(id):
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
+@set_task
+def update_task(task):
     json = request.get_json(force=True)
 
     task.title = json.get('title', task.title)
@@ -83,12 +78,8 @@ def update_task(id):
 
 
 @api_v1.route('/tasks/<id>', methods=['DELETE'])
-def delete_tasks(id):
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
+@set_task
+def delete_tasks(task):
     if task.delete():
         return response(task.serialize())
 
